c0c30a742d3880fa85c0886b6a13b372a5eb12aa
lam-2005 <lam2k5phan@gmail.com>
2026-07-08 17:52:26 +0700

    used socket.io to return data after the Ai analysis

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express, { type Express } from "express";
-
+import cors from "cors";
 import logsRouter from "./routers/logs.route.js";
 
 const app: Express = express();
 
+app.use(cors({ origin: "http://localhost:5000", credentials: true }));
+
 app.use(express.json());
+
 app.use("/api/logs", logsRouter);
+app.get("/", (req, res) => res.send("heello"));
 
 export default app;
--- backend/src/routers/logs.route.ts
+++ backend/src/routers/logs.route.ts
@@ -7,5 +7,8 @@ const router: Router = Router();
 const CLogs = new LogsController();
 
 router.post("/report", CLogs.createLog);
+router.get("/errors", CLogs.getErrors);
+router.get("/errors/:fingerprint", CLogs.getErrorByFingerprint);
+router.post("/errors/:id/retry", CLogs.retry);
 
 export default router;
